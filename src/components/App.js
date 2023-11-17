cfa9521128a333fcb89e05aadd76aa670561ea0f
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2023-11-17 22:55:59 +0530

    complete 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,29 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
     constructor(props) {
         super(props)
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: "0px" }
+            ballPosition: { left: "0px" },
+            currentPosition:0,
+            
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.moveBallRight = this.moveBallRight.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+
     };
 
+  
+    
     buttonClickHandler() {
-   
+    this.setState({
+        renderBall: true
+    });
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
@@ -25,9 +35,33 @@ class App extends Component {
     }
 
     // bind ArrowRight keydown event
-    componentDidMount() {
-      
+
+    moveBallRight() {
+
+       this.setState({currentPosition:this.state.currentPosition+5})
+
+        this.setState({
+            ballPosition: { left: `${this.state.currentPosition}px` }
+        });
+
     }
+
+
+    handleKeyDown(event) {
+        // Check if the right arrow key is pressed (key code 39)
+        if (event.keyCode === 39) {
+           this.moveBallRight();
+        }
+    }
+
+   
+
+    componentDidMount() {
+        
+        document.addEventListener('keydown', this.handleKeyDown);
+    }
+
+    
 
     render() {
         return (
